0d190148059ceeb42ebb7bf11476b9f5df670d9e
Ronit <sharmaronit066@gmail.com>
2022-08-04 19:00:49 +0530

    Update

--- views.sql
+++ views.sql
@@ -6,7 +6,10 @@ FROM clients  c
 JOIN invoices i USING (client_id)
 GROUP BY client_id,name;
 
-CREATE VIEW clients_balance AS 
+
+-- To Update VIEWS 
+
+CREATE OR REPLACE VIEW clients_balance AS 
 SELECT  c.client_id,
 		name,
         SUM(invoice_total) - SUM(payment_total) AS Balance 
