6a00ecf938e075d22fd839286814943aaa601e34
Ronit <sharmaronit066@gmail.com>
2022-08-08 18:11:41 +0530

    Add

---
+++ Events.sql
@@ -0,0 +1,16 @@
+DELIMITER $$ 
+
+CREATE EVENT yearly_delete_stale_audit_rows
+ON SCHEDULE 
+	-- AT '2019-05-01'
+    EVERY 1 YEAR STARTS '2019-06-01' ENDS '2029-09-01'
+DO BEGIN 
+	DELETE FROM payments_audit
+    WHERE action_date < NOW() - INTERVAL 1 YEAR ; 
+END $$
+
+DELIMITER ;
+
+-- SHOW EVENTS ;
+-- DROP EVENT IF EXISTS yearly_delete_stale_audit_rows;
+-- ALTER EVENT yearly_delete_stale_audit_rows DISABLE /ENABLE ; 
